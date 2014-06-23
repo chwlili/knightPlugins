0bca6508a78477f7442d65f5b8ad80508344846f
chw <chw-chengwei@foxmail.com>
2014-06-23 19:10:11 +0800

    代码折叠优化

--- org.chw.game.cfg.ui/src/org/chw/game/ui/CfgUiModule.java
+++ org.chw.game.cfg.ui/src/org/chw/game/ui/CfgUiModule.java
@@ -3,12 +3,14 @@
  */
 package org.chw.game.ui;
 
+import org.chw.game.ui.folder.CfgFolder;
 import org.chw.game.ui.highlight.CfgHighlight;
 import org.chw.game.ui.highlight.CfgHighlightCalculator;
 import org.chw.game.ui.hyperlink.CfgHyperLinkHelper;
 import org.chw.game.ui.hyperlink.CfgLoationInFileProvider;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.eclipse.xtext.resource.ILocationInFileProvider;
+import org.eclipse.xtext.ui.editor.folding.IFoldingRegionProvider;
 import org.eclipse.xtext.ui.editor.syntaxcoloring.IHighlightingConfiguration;
 import org.eclipse.xtext.ui.editor.syntaxcoloring.ISemanticHighlightingCalculator;
 
@@ -41,5 +43,10 @@ public class CfgUiModule extends org.chw.game.ui.AbstractCfgUiModule
 	public Class<? extends ILocationInFileProvider> bindILocationInFileProvider()
 	{
 		return CfgLoationInFileProvider.class;
+	}
+	
+	public Class<? extends IFoldingRegionProvider> bindIFoldingRegionProvider()
+	{
+		return CfgFolder.class;
 	}
 }
---
+++ org.chw.game.cfg.ui/src/org/chw/game/ui/folder/CfgFolder.java
@@ -0,0 +1,64 @@
+package org.chw.game.ui.folder;
+
+import org.chw.game.cfg.CfgPackage;
+import org.chw.game.cfg.Type;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.xtext.nodemodel.INode;
+import org.eclipse.xtext.nodemodel.util.NodeModelUtils;
+import org.eclipse.xtext.ui.editor.folding.DefaultFoldingRegionProvider;
+import org.eclipse.xtext.ui.editor.folding.IFoldingRegionAcceptor;
+import org.eclipse.xtext.util.ITextRegion;
+import org.eclipse.xtext.util.TextRegion;
+
+public class CfgFolder extends DefaultFoldingRegionProvider
+{
+	@Override
+	protected boolean isHandled(EObject eObject) 
+	{
+		if(eObject instanceof Type)
+		{
+			return super.isHandled(eObject);
+		}
+		
+		return false;
+	}
+
+	protected void computeObjectFolding(EObject eObject, IFoldingRegionAcceptor<ITextRegion> foldingRegionAcceptor) 
+	{
+		if(eObject instanceof Type)
+		{
+			Type type=(Type)eObject;
+
+			int from=-1;
+			int to=-1;
+			
+			for(INode node : NodeModelUtils.findNodesForFeature(type,CfgPackage.Literals.TYPE__PREFIX))
+			{
+				from=node.getOffset();
+				break;
+			}
+			
+			INode last=NodeModelUtils.findActualNodeFor(eObject);
+			if(last!=null)
+			{
+				to=last.getOffset()+last.getLength();
+			}
+			
+			if(from!=-1 && to!=-1)
+			{
+				foldingRegionAcceptor.accept(from, to-from, new TextRegion(from, to-from));
+			}
+			
+
+			for(INode node : NodeModelUtils.findNodesForFeature(type,CfgPackage.Literals.TYPE__COMM))
+			{
+				foldingRegionAcceptor.accept(node.getOffset(), node.getLength(), new TextRegion(node.getOffset(), node.getLength()));
+				break;
+			}
+		}
+		else
+		{
+			super.computeObjectFolding(eObject, foldingRegionAcceptor);
+		}
+	}
+}
