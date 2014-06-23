563623e5532018c94ff67799b168e1113cead74c
chengweiwei <chengweiwei@foxmail.com>
2014-06-23 21:46:53 +0800

    格式化程序，大纲视图。

--- org.chw.game.cfg/src/org/chw/game/formatting/CfgFormatter.java
+++ org.chw.game.cfg/src/org/chw/game/formatting/CfgFormatter.java
@@ -11,22 +11,32 @@ import org.chw.game.services.CfgGrammarAccess;
 /**
  * This class contains custom formatting description.
  * 
- * see : http://www.eclipse.org/Xtext/documentation.html#formatting
- * on how and when to use it 
+ * see : http://www.eclipse.org/Xtext/documentation.html#formatting on how and
+ * when to use it
  * 
- * Also see {@link org.eclipse.xtext.xtext.XtextFormattingTokenSerializer} as an example
+ * Also see {@link org.eclipse.xtext.xtext.XtextFormattingTokenSerializer} as an
+ * example
  */
-public class CfgFormatter extends AbstractDeclarativeFormatter {
-	
+public class CfgFormatter extends AbstractDeclarativeFormatter
+{
+
 	@Inject
-	private CfgGrammarAccess grammarAccess; 
-	
+	private CfgGrammarAccess grammarAccess;
+
 	@Override
-	protected void configureFormatting(FormattingConfig c) {
-// It's usually a good idea to activate the following three statements.
-// They will add and preserve newlines around comments
-//		c.setLinewrap(0, 1, 2).before(grammarAccess.getSL_COMMENTRule());
-//		c.setLinewrap(0, 1, 2).before(grammarAccess.getML_COMMENTRule());
-//		c.setLinewrap(0, 1, 1).after(grammarAccess.getML_COMMENTRule());
+	protected void configureFormatting(FormattingConfig c)
+	{
+		c.setLinewrap(2).after(grammarAccess.getTypeRule());
+
+		c.setLinewrap().before(grammarAccess.getFieldRule());
+		c.setLinewrap(2).after(grammarAccess.getFieldRule());
+
+		c.setLinewrap().after(grammarAccess.getCOMMENTRule());
+
+		c.setLinewrap().before(grammarAccess.getC_BRACE_LRule());
+		c.setLinewrap().before(grammarAccess.getC_BRACE_RRule());
+
+		c.setIndentationIncrement().after(grammarAccess.getC_BRACE_LRule());
+		c.setIndentationDecrement().before(grammarAccess.getC_BRACE_RRule());
 	}
 }
